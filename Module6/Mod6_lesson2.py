3c8f965cfbd4420ab94778e723be842fa64eefe5
Theresa Karamor <teriekarie@gmail.com>
2022-08-12 03:12:58 +0100

    Adding comment

--- Module6/Mod6_lesson2.py
+++ Module6/Mod6_lesson2.py
@@ -1,9 +1,13 @@
+from statistics import quantiles
 import numpy
 
 # calculating the Quartile of random values
 values = [12, 44, 60, 45, 20, 44, 34, 50, 45, 90]
 
-# Assigning a variable x
+# Defining the Quantile values
+quantiles = [0,0.25,0.5,0.75,1]
+
 x = numpy.quantile(values, [0,0.25,0.5,0.75,1])
+
 print(x)
 
